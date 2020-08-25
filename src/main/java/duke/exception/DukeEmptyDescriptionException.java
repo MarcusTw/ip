cd7834e9e66c3b286583284b2f24ca71955683ef
MarcusTw <60913700+MarcusTw@users.noreply.github.com>
2020-08-25 21:48:33 +0800

    JavaDoc exception class constructor

--- src/main/java/duke/exception/DukeEmptyDescriptionException.java
+++ src/main/java/duke/exception/DukeEmptyDescriptionException.java
@@ -4,6 +4,11 @@ package duke.exception;
  * Represents an exception thrown when description of a Task is left empty.
  */
 public class DukeEmptyDescriptionException extends DukeTaskException {
+    /**
+     * Class constructor.
+     *
+     * @param task The String representation of the Task.
+     */
     public DukeEmptyDescriptionException(String task) {
         super(String.format("☹ OOPS!!! The description of %s cannot be empty.", task));
     }
